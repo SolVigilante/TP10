c88bd23294ffc0703007ada19b19717677715aba
vigil <vigil@10.2.66.20>
2025-05-28 10:14:44 -0300

    Ej1

--- EJ1/ej1.c
+++ EJ1/ej1.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-	printf("Hello world\n");
+	int cant;
+
+
+	printf("Cuantos enteros quiere resevar?\n");
+	scanf("%d", &cant);
+
+	int * buffer = (int *) calloc(cant*sizeof(int), sizeof(int));
+
+	printf("Buffer creado:");
+	for(int i= 0; i<cant; i++)
+		printf("%d", buffer[0]);
+	putchar('\n');
+
+	free(buffer);
+
+	return 0;
+
 }
