7e23d0a7a8f148ce0a45aa865711c75dd41ec7ef
vigil <vigil@10.2.66.20>
2025-05-28 10:48:31 -0300

    Ejercicio 2

--- EJ2/ej2_1.c
+++ EJ2/ej2_1.c
@@ -3,12 +3,30 @@
 #include <time.h>
 
 int main(){
-	double random;
+	double random_0_1;
+	double random_0_n;
+	double random_n_m;
+
+	double n;
+	double m;
+
 	srand(time(NULL));
 
-	random = (double)rand()/ (RAND_MAX * (0.1-0.0 +1.0));
+	printf("Cual es el numero n?\n");
+	scanf("%lf", &n);
 
-	printf("Número random: %f\n", random);
+	printf("Cual es el numero m?\n");
+	scanf("%lf", &m);
+
+	random_0_1 = (double)rand()/ (double)RAND_MAX;
+
+	random_0_n = ((double)rand()/ (double)RAND_MAX) * (n - 0.0);
+
+	random_n_m = ((double)rand()/ (double)RAND_MAX) * (n - m) + m;
+
+	printf("Número random 0-1: %f\n", random_0_1);
+	printf("Número random 0-n: %f\n", random_0_n);
+	printf("Número random n-m: %f\n", random_n_m);
 
 	return 0;
 }
